f8e9ba16cbec6b2cffee41ebe0036302416411d9
s3rxio <cergotabagua222@gmail.com>
2024-09-10 01:40:59 +0300

    build(app): fix frontend env on build

--- front/src/shared/configs.ts
+++ front/src/shared/configs.ts
@@ -5,5 +5,5 @@ export const catsApiConfig = {
 };
 
 export const appApiConfig = {
-  url: import.meta.env.VITE_APP_API_URL || "http://localhost:3000",
+  url: import.meta.env.VITE_APP_API_URL || `${window.location.origin}/api`,
 };
